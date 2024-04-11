34dff9b6fbae0be2075f92394f62eade05f3a689
PratikBorge <159998726+PratikBorge@users.noreply.github.com>
2024-04-11 17:33:54 +0530

    Update main.tf

--- TerraformModule/Resources/EKS/main.tf
+++ TerraformModule/Resources/EKS/main.tf
@@ -3,13 +3,13 @@ resource "aws_eks_cluster" "eks_cluster" {
     vpc_config {
       subnet_ids = var.public_subnet_ids 
   }
-    role_arn = "arn:aws:iam::471112957025:role/eks1"
+    role_arn = "arn:aws:iam::471112957025:role/eks1" //use role created for eks cluster
 }
 
 resource "aws_eks_node_group" "eks_node_group" {
     cluster_name = aws_eks_cluster.eks_cluster.name
     subnet_ids = var.public_subnet_ids
-    node_role_arn = "arn:aws:iam::471112957025:role/eks2"
+    node_role_arn = "arn:aws:iam::471112957025:role/eks2" //use role created for eks cluster nodegroup
     scaling_config {
        desired_size = 1
        max_size     = 2
