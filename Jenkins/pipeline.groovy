df7a00cc97b1b89b195ac3f291520f9efc0b19d8
PratikBorge <159998726+PratikBorge@users.noreply.github.com>
2024-04-11 17:25:28 +0530

    Update pipeline.groovy

--- Jenkins/pipeline.groovy
+++ Jenkins/pipeline.groovy
@@ -8,19 +8,14 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                git branch: 'main', credentialsId: '37', url: 'https://github.com/PratikBorge/THREEMODULE.git'
-                //sh'''
-                //sudo apt update -y
-               // sudo apt install git -y
-               // sudo git clone 'https://github.com/PratikBorge/studentapp.git'
-               // '''
+                git branch: 'main', credentialsId: '<credentials_id>', url: '<github_repo_link>'
                 dir('./studentapp') {
                     sh'''
                     sudo apt update -y
                     sudo mvn clean package
                     sudo mv ./target/*.war ./target/student.war
                     sudo snap install aws-cli --classic
-                    sudo aws s3 cp ./target/student.war s3://artifactforthreeprat --acl public-read
+                    sudo aws s3 cp ./target/student.war <s3bucket_url> --acl public-read
                     '''
                 }
             }
@@ -32,36 +27,20 @@ pipeline {
         }
         stage('Deploy') {
             steps {
-                //dir('/home/ubuntu/workspace/ThreeTier'){
-                  //  sh'''
-                    //sudo rm -rf *
-                   // '''
-                //git branch: 'main', credentialsId: '37', url: 'https://github.com/PratikBorge/THREEMODULE.git'
                 dir('./Docker/Backend') {
-                    //script{
-                  //  withDockerRegistry(credentialsId: '38', toolName: 'Docker'){
                     sh'''
-                    sudo docker login -u pratikborge -p Pratik@022
-                    sudo docker build -t pratikborge/backend .
+                    sudo docker login -u <docker_hub_username> -p <docker_hub_password>
+                    sudo docker build -t pratikborge/backend . //same as your dockerhub repo
                     sudo docker push pratikborge/backend
                     sh'''
                 }
                 dir('./Docker/Frontend') {
-                    //sudo withDockerRegistry(credentialsId: '38', url: '') {
                     sh'''
-                    sudo docker login -u pratikborge -p Pratik@022
-                    sudo docker build -t pratikborge/frontend .
+                    sudo docker login -u <docker_hub_username> -p <docker_hub_password>
+                    sudo docker build -t pratikborge/frontend . //same as your dockerhub repo
                     sudo docker push pratikborge/frontend
                     '''
                 }
-                //dir('./Docker/Database') {
-                    //sudo withDockerRegistry(credentialsId: '38', url: '') {
-                   // sh'''
-                  //  sudo docker login -u pratikborge -p Pratik@022
-                   // sudo docker build -t pratikborge/database .
-                   // sudo docker push pratikborge/database
-                   // '''
-               // }
             }
         }
     }
