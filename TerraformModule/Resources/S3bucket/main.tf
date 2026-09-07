f94a769aedebcc8bdfbe434df0e70af57ed35c24
Pratik <pratikborge37@gmail.com>
2026-09-07 15:52:04 +0530

    new update

--- TerraformModule/Resources/S3bucket/main.tf
+++ TerraformModule/Resources/S3bucket/main.tf
@@ -32,5 +32,5 @@ resource "aws_s3_bucket_acl" "acl" {
 resource "aws_s3_object" "artifactforthreeprat" {
   bucket = aws_s3_bucket.artifactforthreeprat.bucket 
   key    = "mysql-connector.jar"
-  source = "/mnt/c/Users/pratik/Desktop/Webapps/mysql-connector.jar"
+  source = "/home/rocky/ThreeTierArchitecture/studentapp/mysql-connector.jar"
 }
